4816c805f97552ccf879fd8837ba9ba5dc6001eb
codefulTM <147736560+codefulTM@users.noreply.github.com>
2025-12-15 04:43:45 +0700

    fixed invalid namespace error

--- src/gateways/gateways.module.ts
+++ src/gateways/gateways.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SeatStatusGateway } from './seat-status.gateway';
+import { BookingGateway } from './booking.gateway';
 import { SeatStatus } from '../entities/seat-status.entity';
 
 @Module({
-    imports: [ConfigModule, TypeOrmModule.forFeature([SeatStatus])],
-    providers: [SeatStatusGateway],
-    exports: [SeatStatusGateway],
+  imports: [ConfigModule, TypeOrmModule.forFeature([SeatStatus])],
+  providers: [SeatStatusGateway, BookingGateway],
+  exports: [SeatStatusGateway, BookingGateway],
 })
 export class GatewaysModule {
-    constructor(private readonly seatStatusGateway: SeatStatusGateway) {
-        // Start the lock cleanup process
-        this.seatStatusGateway.startLockCleanup();
-    }
+  constructor(
+    private readonly seatStatusGateway: SeatStatusGateway,
+    private readonly bookingGateway: BookingGateway,
+  ) {
+    // Start the lock cleanup process
+    this.seatStatusGateway.startLockCleanup();
+  }
 }
