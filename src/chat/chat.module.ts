9394efc048c033aea093141ded1a209b0c697565
codefulTM <147736560+codefulTM@users.noreply.github.com>
2025-12-21 21:44:51 +0700

    fixed bug

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,11 +4,12 @@ import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { AiService } from '../ai/ai.service';
 import { ChatToolsService } from './chat-tools.service';
+import { TripsModule } from '../trips/trips.module';
 import { Conversation } from './entities/conversation.entity';
 import { Message } from './entities/message.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Conversation, Message])],
+  imports: [TypeOrmModule.forFeature([Conversation, Message]), TripsModule],
   controllers: [ChatController],
   providers: [ChatService, AiService, ChatToolsService],
   exports: [ChatService],
